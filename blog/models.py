9fb143b08b265687e7f019e9c5f94cdc2e21c716
Suhyeon Kim <kimsuehune@gmail.com>
2024-03-29 13:42:37 +0900

    first commit

---
+++ blog/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+class Blog(models.Model):
+    title = models.CharField(max_length=20)
+    content = models.TextField()
+    writer = models.CharField(max_length=20)
+    createdAt = models.DateTimeField(auto_now_add=True)
+    email = models.EmailField()
+    team = models.CharField(max_length=20)
+    is_published = models.BooleanField(default=False)
+
+
+    def __str__(self):
+        return self.title
+
+# Create your models here.
